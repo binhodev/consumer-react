46586c1ed01c1e554258b30e8b3749e72256ddaa
Binho.dev <silvinhofranca.dev@icloud.com>
2025-02-05 14:28:34 -0300

    feat(hooks): alterar método de requisição assíncrona de asyncGet para get

--- packages/next/src/hooks/use.get.ts
+++ packages/next/src/hooks/use.get.ts
@@ -19,7 +19,7 @@ export function useGet<T>(
         setIsLoading(true);
         
         client
-            .asyncGet(route)
+            .get(route)
             .then((response) => {
                 const parsedData = schema.parse(response.data);
 

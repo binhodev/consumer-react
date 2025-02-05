dd50d7de3e8979f4417edf2b59d1170aaa77b829
Binho.dev <silvinhofranca.dev@icloud.com>
2025-02-05 10:27:57 -0300

    feat(types): adicionar interfaces para configuração e plugins do ConsumerReactClient

--- packages/core/src/types/types.ts
+++ packages/core/src/types/types.ts
@@ -1,1 +1,35 @@
-import {} from "";
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+
+export interface ConsumerReactClientOptions {
+    baseUrl: string;
+    timeout?: number;
+    plugins?: ConsumerReactPlugin[];
+    interceptors?: {
+        beforeRequest?: (
+            config: AxiosRequestConfig
+        ) => AxiosRequestConfig | Promise<AxiosRequestConfig>;
+        afterResponse?: (
+            response: AxiosResponse
+        ) => AxiosResponse | Promise<AxiosResponse>;
+    };
+}
+
+export interface ConsumerReactError extends Error {
+    status?: number;
+    code?: string;
+    data?: any;
+    originalError?: any;
+}
+
+export interface ConsumerReactPlugin {
+    name: string;
+    beforeRequest?: (
+        config: AxiosRequestConfig
+    ) => AxiosRequestConfig | Promise<AxiosRequestConfig>;
+    afterResponse?: (
+        config: AxiosResponse
+    ) => AxiosResponse | Promise<AxiosResponse>;
+    onError?(
+        error: ConsumerReactError
+    ): ConsumerReactError | Promise<ConsumerReactError>;
+}
